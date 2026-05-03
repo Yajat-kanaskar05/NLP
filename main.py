f564d9b2d7ccdd86978e9b7b3aaff33248ee0859
Yajat-kanaskar05 <yajat.important@gmail.com>
2026-05-03 18:13:36 +0530

    Establish connection of dialogflow and main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message" : "hello world"}
+@app.post('/')
+async def handle_request(request: Request):
+    
+    #Retrieve json data
+    payload = await request.json()
+
+    #extract necessary info from payload
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+
+    if intent == "Track.Order - context : ongoing-tracking":
+        return JSONResponse(content={
+            "fulfillmentText": f"Received intent: {intent}" 
+        })
+
+    # Fallback for unhandled intents
+    return JSONResponse(content={
+        "fulfillmentText": "I received your request but don't have a handler for it."
+    })
+
+    
+        
