e3b391de83d2113352ba4cf3b2409569e6662041
Yajat-kanaskar05 <yajat.important@gmail.com>
2026-05-13 17:50:04 +0530

    Entire backend

--- generic_helper.py
+++ generic_helper.py
@@ -1,18 +1,15 @@
+
 import re
 
+def get_str_from_food_dict(food_dict: dict):
+    result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+    return result
+
+
 def extract_session_id(session_str: str):
-
-    match = re.search(r"/sessions/(.*?)/contexts/" , session_str)
-
+    match = re.search(r"/sessions/(.*?)/contexts/", session_str)
     if match:
-        extracted_string = match.group(1)
+        extracted_string = match.group(0)
         return extracted_string
-    
+
     return ""
-
-def get_str_from_food_dict(food_dict : dict):
-    return ", ".join([f"{int(value)} {key}" for key , value in food_dict.items()])
-
-if __name__ == "__main__":
-    # print(extract_session_id("projects/first-agent-opba/agent/sessions/b88ff248-b608-48ce-7304-c3c8e86f167f/contexts/ongoing-tracking"))
-    print(get_str_from_food_dict({"burger": 2 , "fries": 3}))
